406cc82566a22725df433cbdc778f77341769063
COUNTER <COUNTER@192.168.1.5>
2019-02-28 20:40:30 -0500

    Programacion examen

---
+++ src/ec/edu/ister/contralador/main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ister.contralador;
+
+import ec.edu.ister.modelo.Pedido;
+
+/**
+ *
+ * @author LUIS
+ */
+public class main {
+    
+    public static void main(String[] args) {
+        Pedido pedido = new Pedido(2, 1);
+        
+    }
+    
+}
